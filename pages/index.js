3d90a9ee9e546362dbd97384327c96aab5b203de
Roman Brito <romanbrito1@gmail.com>
2018-12-02 00:45:49 -0600

    adding Link from next/link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import './index.css';
 import Card from './Card'
 
 export default () => (
   <div className="App">
     <header className="App-header">
-      <img className="static-logo" src="/static/logo.png" alt="logo"/>
+      <Link href="/page2"><img className="static-logo" src="/static/logo.png" alt="logo"/></Link>
     </header>
     <div className="Grid">
       <Card/>
